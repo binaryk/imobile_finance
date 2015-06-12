664b680455ff695118372f946f0f5b09685ba708
unknown <calin.verdes@comptech.ro>
2015-06-12 08:39:14 +0300

    Cautare apartamente. Grid

--- app/~imobiliare/grid/apartamente/ApartamenteRecord.php
+++ app/~imobiliare/grid/apartamente/ApartamenteRecord.php
@@ -14,7 +14,7 @@ class ApartamenteRecord extends \Imobiliare\GridsRecord
         $this->toolbar        = 'apartamente.cautare.toolbar';
         $this->name           = 'dt';
         $this->display_start  = 0;
-        $this->display_length = 5;
+        $this->display_length = 25;
         $this->default_order  = "0,'asc'";
         $this->form           = NULL;
         $this->css            = 'admin/css/dt/dt.css, admin/css/dt/toolbar.css, admin/css/dt/dtform.css';
@@ -38,7 +38,7 @@ class ApartamenteRecord extends \Imobiliare\GridsRecord
                 'orderable' => 'no',
                 'class'     => 'td-record-count td-align-center',
                 'visible'   => 'yes',
-                'header'    => ['caption' => '#', 'style'   => 'width:5%',],
+                'header'    => ['caption' => '#', 'style'   => 'width:5%;text-align:center',],
                 'type'      => 'row-number',
                 'source'    => 'row-number',
             ], 
@@ -56,11 +56,29 @@ class ApartamenteRecord extends \Imobiliare\GridsRecord
                 'orderable' => 'no',
                 'class'     => 'td-record-count td-align-left',
                 'visible'   => 'yes',
-                'header'    => ['caption' => 'Adresa', 'style'   => 'width:85%',],
+                'header'    => ['caption' => 'Adresa', 'style'   => 'width:20%',],
                 'type'      => 'field',
                 'source'    => 'adresa_exacta',
             ], 
             '4' => [
+                'id'        => 'nr-camere',
+                'orderable' => 'no',
+                'class'     => 'td-record-count td-align-right',
+                'visible'   => 'yes',
+                'header'    => ['caption' => 'Numar de camere', 'style'   => 'width:5%',],
+                'type'      => 'field-int',
+                'source'    => 'nr_camere',
+            ],
+            '5' => [
+                'id'        => 'pret-m2',
+                'orderable' => 'no',
+                'class'     => 'td-record-count td-align-right',
+                'visible'   => 'yes',
+                'header'    => ['caption' => 'Pret', 'style'   => 'width:5%',],
+                'type'      => 'field-float',
+                'source'    => 'pret_m2',
+            ], 
+            '6' => [
                 'id'        => 'is-agentie',
                 'orderable' => 'no',
                 'class'     => 'td-record-count td-align-center',
@@ -69,9 +87,63 @@ class ApartamenteRecord extends \Imobiliare\GridsRecord
                 'type'      => 'field',
                 'source'    => 'is_agentie',
             ], 
+            '7' => [
+                'id'        => 'ultima-actualizare',
+                'orderable' => 'no',
+                'class'     => 'td-record-count td-align-center',
+                'visible'   => 'yes',
+                'header'    => ['caption' => 'Ultima actualizare', 'style'   => 'width:10%',],
+                'type'      => 'field-date',
+                'source'    => 'ultima_actualizare',
+            ], 
+            '8' => [
+                'id'        => 'telefon',
+                'orderable' => 'no',
+                'class'     => 'td-record-count td-align-left',
+                'visible'   => 'yes',
+                'header'    => ['caption' => 'Telefoane', 'style'   => 'width:15%',],
+                'type'      => 'view',
+                'source'    => 'apartamente.cautare.telefoane',
+            ], 
+            '9' => [
+                'id'        => 'credit-prima-casa',
+                'orderable' => 'no',
+                'class'     => 'td-record-count td-align-center',
+                'visible'   => 'yes',
+                'header'    => ['caption' => 'Credit prima casa', 'style'   => 'width:5%',],
+                'type'      => 'field',
+                'source'    => 'credit_prima_casa',
+            ], 
+            '10' => [
+                'id'        => 'nr-etaje',
+                'orderable' => 'no',
+                'class'     => 'td-record-count td-align-right',
+                'visible'   => 'yes',
+                'header'    => ['caption' => 'Numar de etaje', 'style'   => 'width:5%',],
+                'type'      => 'field-int',
+                'source'    => 'nr_etaj',
+            ],
+            '11' => [
+                'id'        => 'tip-finisaje-interioare',
+                'orderable' => 'no',
+                'class'     => 'td-record-count td-align-left',
+                'visible'   => 'yes',
+                'header'    => ['caption' => 'Finisaj interioar', 'style'   => 'width:10%',],
+                'type'      => 'field',
+                'source'    => 'finisaj_interior',
+            ],
+            '12' => [
+                'id'        => 'tip-compartiment',
+                'orderable' => 'no',
+                'class'     => 'td-record-count td-align-left',
+                'visible'   => 'yes',
+                'header'    => ['caption' => 'Tip compartiment', 'style'   => 'width:10%',],
+                'type'      => 'field',
+                'source'    => 'tip_compartiment',
+            ],
         ];  
         $this->fields = [
-            'fields'      => 'v_apartamente.id,v_apartamente.oferta_valabila,v_apartamente.adresa_exacta,v_apartamente.is_agentie',
+            'fields'      => 'v_apartamente.id,v_apartamente.oferta_valabila,v_apartamente.adresa_exacta,v_apartamente.is_agentie,v_apartamente.nr_camere',
             'searchables' => 'v_apartamente.oferta_valabila',
             'orderables'  => [],
         ];
