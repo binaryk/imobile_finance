d1688ef5ee5a0e0988f1299f90c181733edc431d
unknown <calin.verdes@comptech.ro>
2015-06-24 14:10:21 +0300

    Completarea PDF-ului oferta de apartament

--- app/~imobiliare/apartamente/CreateOfertaPdf.php
+++ app/~imobiliare/apartamente/CreateOfertaPdf.php
@@ -119,6 +119,21 @@ class CreateOfertaPdf
 		]);
 	}
 
+	protected function zonaAproximativaApartament()
+	{
+		return ($zona = $this->zona_aproximativa()) ? $zona : '-';	
+	}
+
+	protected function proprietarApartament()
+	{
+		return $this->apartament->proprietar ? $this->apartament->numeproprietar : '-';
+	}
+
+	protected function telefonProprietarApartament()
+	{
+		return $this->apartament->proprietar ? $this->apartament->telefonproprietar : '-';
+	}
+
 	protected function suprafataApartament()
 	{
 		return ($s = $this->suprafata()) ? _toFloat($s) . ' mp' : '-';
@@ -176,28 +191,28 @@ class CreateOfertaPdf
 
 	protected function outdetails()
 	{
-		$rows = [
-			'1' => ['caption' => 'Adresa: ', 'source' => 'adresa'],
-			'2' => ['caption' => 'Suprafaţa:', 'source' => 'suprafataApartament'],
-			'3' => ['caption' => 'Loc parcare: ', 'source' => 'locparcare'],
-			'4' => ['caption' => 'Etaj: ', 'source' => 'etaj'],
-			'5' => ['caption' => 'Număr de balcoane: ', 'source' => 'balcoane'],
-			'6' => ['caption' => 'Grad de finisare: ', 'source' => 'finisare'],
-			'7' => ['caption' => 'Garaj: ', 'source' => 'garaj'],
-			'8' => ['caption' => 'Centrală termică: ', 'source' => 'centralatermica'],
-			'9' => ['caption' => 'Compartimentare: ', 'source' => 'compartimentare'],
-		   '10' => ['caption' => 'Număr de băi: ', 'source' => 'bai'],
-		   '11' => ['caption' => 'Număr de camere: ', 'source' => 'camere'],
-		   // '12' => ['caption' => 'Detalii: ', 'source' => 'detaliiApartament'],
-		];
+		// $rows = [
+		// 	// '1' => ['caption' => 'Adresa: ', 'source' => 'adresa'],
+		// 	'2' => ['caption' => 'Suprafaţa:', 'source' => 'suprafataApartament'],
+		// 	'3' => ['caption' => 'Loc parcare: ', 'source' => 'locparcare'],
+		// 	'4' => ['caption' => 'Etaj: ', 'source' => 'etaj'],
+		// 	'5' => ['caption' => 'Număr de balcoane: ', 'source' => 'balcoane'],
+		// 	'6' => ['caption' => 'Grad de finisare: ', 'source' => 'finisare'],
+		// 	'7' => ['caption' => 'Garaj: ', 'source' => 'garaj'],
+		// 	'8' => ['caption' => 'Centrală termică: ', 'source' => 'centralatermica'],
+		// 	'9' => ['caption' => 'Compartimentare: ', 'source' => 'compartimentare'],
+		//    '10' => ['caption' => 'Număr de băi: ', 'source' => 'bai'],
+		//    '11' => ['caption' => 'Număr de camere: ', 'source' => 'camere'],
+		//    // '12' => ['caption' => 'Detalii: ', 'source' => 'detaliiApartament'],
+		// ];
 
-		foreach($rows as $i => $row)
-		{
-			$this->pdf->Pdf()->SetFont('freeserif', 'B', 12, '', false);
-			$this->pdf->Cell()->text( $row['caption'] )->width(50)->border('')->halign('R')->linefeed(0)->out();
-			$this->pdf->Pdf()->SetFont('freeserif', '', 12, '', false);
-			$this->pdf->Cell()->text( $this->{$row['source']}() )->width(140)->border('')->halign('L')->linefeed(1)->out();
-		}
+		// foreach($rows as $i => $row)
+		// {
+		// 	$this->pdf->Pdf()->SetFont('freeserif', 'B', 12, '', false);
+		// 	$this->pdf->Cell()->text( $row['caption'] )->width(50)->border('')->halign('R')->linefeed(0)->out();
+		// 	$this->pdf->Pdf()->SetFont('freeserif', '', 12, '', false);
+		// 	$this->pdf->Cell()->text( $this->{$row['source']}() )->width(140)->border('')->halign('L')->linefeed(1)->out();
+		// }
 
 		if( $detalii = $this->detalii()) // afisarea campului detalii
 		{
@@ -209,15 +224,244 @@ class CreateOfertaPdf
 
 	protected function outfooter()
 	{
+		$this->pdf->Pdf()->SetFont('freeserif', '', 11, '', false);
 		$this->pdf->Pdf()->SetXY(10, -25);
-		$this->pdf->Cell()->text('Pentru informaţii suplimentare sau alte oferte imobiliare mă puteţi contacta la telefon 0744332116, pe email ioana.biris@creditfin.ro sau la sediul nostru pe str. Constantin Brâncuşi, nr. 85, etaj 1, Cluj-Napoca')->width(190)->border('T')->halign('C')->out();
+		$this->pdf->Cell()->text('Pentru informaţii suplimentare sau alte oferte imobiliare mă puteţi contacta la telefon 0744332116, pe email ioana.biris@creditfin.ro sau la sediul nostru pe str. Constantin Brâncuşi, nr. 85, etaj 1, Cluj-Napoca')->width(190)->border('T')->halign('C')->out()->reset('border')->reset('width')->reset('halign');
+	}
+
+	protected function outChapterLocalizare()
+	{
+		$this->pdf->Pdf()->SetFont('freeserif', 'B', 14, '', false);
+		$this->pdf->Cell()->text('I. Localizare')->width(190)->border('TB')->halign('L')->background([210, 210, 210])->out()->reset('background');
+		$rows = [
+			'1' => ['caption' => 'Adresa: ', 'source' => 'adresa'],
+			'2' => ['caption' => 'Zona aproximativă: ', 'source' => 'zonaAproximativaApartament'],
+		];
+		$this->pdf->Pdf()->ln();
+		foreach($rows as $i => $row)
+		{
+			$this->pdf->Pdf()->SetFont('freeserif', 'B', 12, '', false);
+			$this->pdf->Cell()->text( $row['caption'] )->width(50)->border('')->halign('R')->linefeed(0)->out();
+			$this->pdf->Pdf()->SetFont('freeserif', '', 12, '', false);
+			$this->pdf->Cell()->text( $this->{$row['source']}() )->width(140)->border('')->halign('L')->linefeed(1)->out();
+		}
+	}
+
+	protected function outChapterProprietar()
+	{
+		$this->pdf->Pdf()->SetFont('freeserif', 'B', 14, '', false);
+		$this->pdf->Cell()->text('II. Proprietar')->width(190)->border('TB')->halign('L')->background([210, 210, 210])->out()->reset('background');
+		$rows = [
+			'1' => ['caption' => 'Proprietar: ', 'source' => 'proprietarApartament'],
+			'2' => ['caption' => 'Telefon: ', 'source' => 'telefonProprietarApartament'],
+		];
+		foreach($rows as $i => $row)
+		{
+			$this->pdf->Pdf()->SetFont('freeserif', 'B', 12, '', false);
+			$this->pdf->Cell()->text( $row['caption'] )->width(50)->border('')->halign('R')->linefeed(0)->out();
+			$this->pdf->Pdf()->SetFont('freeserif', '', 12, '', false);
+			$this->pdf->Cell()->text( $this->{$row['source']}() )->width(140)->border('')->halign('L')->linefeed(1)->out();
+		}
+	}
+
+	protected function numeApartament()
+	{
+		return ($nume = $this->nume()) ? $nume : '-';
+	}
+
+	protected function emailApartament()
+	{
+		return ($email = $this->email()) ? $email : '-';
+	}
+
+	protected function telefoaneApartament()
+	{
+		return \Easy\Form\StringHelper::Items([
+			$this->telefon(), 
+			$this->telefon_secundar_1(), 
+			$this->telefon_secundar_2()
+		]);	
+	}
+
+	protected function numarCamereApartament()
+	{
+		return ($n = $this->nr_camere()) ? _toInt($n) : '-';
+	}
+
+	protected function numarBaiApartament()
+	{
+		return ($n = $this->nr_bai()) ? _toInt($n) : '-';
+	}
+
+	protected function numarBalcoaneApartament()
+	{
+		$d = $this->detalii_bacoane();
+		return (($n = $this->apartament->numetipbalcon) ? $n : '-') . ($d ? '. ' . $d : '');
+	}
+
+	protected function anConstructieApartament()
+	{
+		return ($n = $this->anul_constructiei()) ? $n : '-';
+	}
+
+	protected function etajApartament()
+	{
+		return $this->apartament->numetipetaj ? $this->apartament->numetipetaj : '-';
+	}
+
+	protected function acoperisApartament()
+	{
+		return $this->apartament->numetipacoperis ? $this->apartament->numetipacoperis : '-';
+	}
+
+	protected function confortApartament()
+	{
+		return $this->apartament->numetipconfort ? $this->apartament->numetipconfort : '-';
+	}
+
+	protected function sistemIncalzireApartament()
+	{
+		return $this->id_sistem_incalzire() ? $this->id_sistem_incalzire() : '-';
+	}
+
+	protected function garajApartament()
+	{
+		return $this->apartament->numetipgaraj ? $this->apartament->numetipgaraj : '-';
+	}
+
+	protected function tipCladireApartament()
+	{
+		return $this->apartament->numetipcladire ? $this->apartament->numetipcladire : '-';
+	}
+
+	protected function finisajeApartament()
+	{
+		return $this->apartament->numetipfinisare ? $this->apartament->numetipfinisare : '-';
+	}
+
+	protected function compartimentareApartament()
+	{
+		return $this->apartament->numetipcompartimentare ? $this->apartament->numetipcompartimentare : '-';
+	}
+
+	protected function esteAgentie()
+	{
+		return $this->is_agentie() ? 'DA' : 'NU';
+	}
+
+	protected function ofertaValabila()
+	{
+		return $this->oferta_valabila() ? 'DA' : 'NU';
+	}
+
+	protected function suprafataminmaxApartament()
+	{
+		return \Easy\Form\StringHelper::Items([
+			($s = $this->suprafata()) ? _toFloat($s) . ' mp' : '',
+			($s = $this->suprafata_min()) ? 'Min: ' . _toFloat($s) . ' mp' : '',
+			($s = $this->suprafata_max()) ? 'Max: ' . _toFloat($s) . ' mp' : '',
+		]);	
+	}
+
+	protected function pretApartament()
+	{
+		return ($p = $this->pret_m2()) ? _toFloat($p) . ' LEI, ' . ($this->negociabil() ? 'negociabil' : 'nenegociabil') : '';
+	}
+
+	protected function ultimaActualizareApartament()
+	{
+		$u = $this->ultima_actualizare();
+		return ($u && ($u != '0000-00-00')) ? _toDate($u) : _toDate( $this->created_at());
+	}
+
+	protected function outChapterDategenerale()
+	{
+		$this->pdf->Pdf()->SetFont('freeserif', 'B', 14, '', false);
+		$this->pdf->Cell()->text('III. Date generale')->width(190)->border('TB')->halign('L')->background([210, 210, 210])->out()->reset('background');
+		$rows = [
+			'1' => ['caption' => 'Nume: ', 'source' => 'numeApartament'],
+			'2' => ['caption' => 'Email: ', 'source' => 'emailApartament'],
+			'3' => ['caption' => 'Telefoane: ', 'source' => 'telefoaneApartament'],
+			'4' => ['caption' => 'Este agenţie: ', 'source' => 'esteAgentie'],
+			'5' => ['caption' => 'Oferta valabilă: ', 'source' => 'ofertaValabila'],
+			'6' => ['caption' => 'Anul construcţiei: ', 'source' => 'anConstructieApartament'],
+			'7' => ['caption' => 'Număr de camere: ', 'source' => 'numarCamereApartament'],
+			'8' => ['caption' => 'Suprafaţă: ', 'source' => 'suprafataminmaxApartament'],
+			'9' => ['caption' => 'Număr de băi: ', 'source' => 'numarBaiApartament'],
+		   '10' => ['caption' => 'Număr de balcoane: ', 'source' => 'numarBalcoaneApartament'],
+		   '11' => ['caption' => 'Etaj: ', 'source' => 'etajApartament'],
+		   '12' => ['caption' => 'Acoperiş: ', 'source' => 'acoperisApartament'],
+		   '13' => ['caption' => 'Confort: ', 'source' => 'confortApartament'],
+		   '14' => ['caption' => 'Sistemul de încălzire: ', 'source' => 'sistemIncalzireApartament'],
+		   '15' => ['caption' => 'Garaj: ', 'source' => 'garajApartament'],
+		   '16' => ['caption' => 'Tipul de clădire: ', 'source' => 'tipCladireApartament'],
+		   '17' => ['caption' => 'Tipul de finisaj interior: ', 'source' => 'finisajeApartament'],
+		   '18' => ['caption' => 'Compartimentare: ', 'source' => 'compartimentareApartament'],
+		   '19' => ['caption' => 'Preţ: ', 'source' => 'pretApartament'],
+		   '20' => ['caption' => 'Actualizat la: ', 'source' => 'ultimaActualizareApartament'],		
+		];
+		foreach($rows as $i => $row)
+		{
+			$this->pdf->Pdf()->SetFont('freeserif', 'B', 12, '', false);
+			$this->pdf->Cell()->text( $row['caption'] )->width(50)->border('')->halign('R')->linefeed(0)->out();
+			$this->pdf->Pdf()->SetFont('freeserif', '', 12, '', false);
+			$this->pdf->Cell()->text( $this->{$row['source']}() )->width(140)->border('')->halign('L')->linefeed(1)->out();
+		}
+	}
+
+	protected function outChapterOptiunisuplimentare()
+	{
+		$this->pdf->Pdf()->ln();
+		$this->pdf->Pdf()->SetFont('freeserif', 'B', 14, '', false);
+		$this->pdf->Cell()->text('IV. Opţiuni suplimentare')->width(190)->border('TB')->halign('L')->background([210, 210, 210])->out()->reset('background')->border('TBRL');
+		$items = [
+			'termopan' => 'Termopan',
+			'contoare_gaz' => 'Contoare gaz',
+			'parchet' => 'Parchet',
+			'faianta' => 'Faianţă',
+			'aer_conditionat' => 'Aer condiţionat',
+			'uscator' => 'Uscător',
+			'centrala_termica' => 'Centrală termică',
+			'contoare_apa' => 'Contoare apă',
+			'zugravit_lavabil' => 'Zugrăvit lavabil',
+			'tv_cablu' => 'TV prin cablu',
+			'loc_pod' => 'Loc pod',
+			'usa_atiefractie' => 'Uşă antiefracţie',
+			'modificari_interioare' => 'Modificări interioare',
+			'gresie' => 'Gresie',
+			'balcoane_inchise' => 'Balcoane închise',
+			'has_telefon' => 'Telefon',
+			'loc_pivnita' => 'Loc pivniţă',
+			'parcare' => 'Parcare',
+		];
+
+		$i = 0;
+		foreach($items as $field => $item)
+		{	
+			$i++;
+			$this->pdf->Pdf()->SetFont('freeserif', 'B', 12, '', false);
+			$this->pdf->Cell()->width(70)->text($item . ' ')->linefeed(0)->halign('R')->out();
+			$this->pdf->Pdf()->SetFont('freeserif', '', 12, '', false);
+			$value = $this->$field() ? 'DA' : '-';
+			$this->pdf->Cell()->width(25)->text($value)->halign('C')->linefeed( (int)($i%2 == 0) )->out();
+		}
 	}
 
 	protected function createpdf()
 	{
+		// Pagina 1
 		$this->outlogo();
 		$this->outnume();
 		$this->outphotos();
+		$this->outChapterLocalizare(); 
+		$this->outChapterProprietar(); 
+		$this->outChapterDategenerale();
+		$this->outfooter();
+		// Pagina 2
+		$this->pdf->newpage($this->orientation, $this->pagesize);
+		$this->outlogo();
+		$this->outnume();
+		$this->outChapterOptiunisuplimentare();
 		$this->outdetails();
 		$this->outfooter();
 	}
