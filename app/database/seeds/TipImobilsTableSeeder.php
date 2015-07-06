3b997cad5a04d9674dc1ffea4b92078b0b150ed2
binaryk <lupacescueduard@yahoo.com>
2015-07-06 14:12:21 +0300

    Id-uri in seeds

--- app/database/seeds/TipImobilsTableSeeder.php
+++ app/database/seeds/TipImobilsTableSeeder.php
@@ -10,11 +10,12 @@ class TipImobilsTableSeeder extends Seeder {
 		DB::table('tip_imobile')->delete();
 		$faker = Faker::create(); 
 		Imobiliare\Nomenclator\TipImobil::insert([
-			[ 'nume' => 'Apartament' ],
-			[ 'nume' => 'Garsoniera' ],
-			[ 'nume' => 'Casa' ],
-			[ 'nume' => 'Duplex' ],
-			[ 'nume' => 'Teren' ]
+			['id' => 1, 'nume' => 'Apartament' ],
+			['id' => 2, 'nume' => 'Garsoniera' ],
+			['id' => 3, 'nume' => 'Casa' ],
+			['id' => 4, 'nume' => 'Duplex' ],
+			['id' => 5, 'nume' => 'Teren' ],
+			['id' => 6, 'nume' => 'Vila' ],
 		]);
 	}
 
