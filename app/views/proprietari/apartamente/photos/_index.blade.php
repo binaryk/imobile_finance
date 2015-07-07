d0885d10b80f54d9058e82c787b4149f6b700dda
binaryk <lupacescueduard@yahoo.com>
2015-07-07 17:19:08 +0300

    Test server photos

--- app/views/proprietari/apartamente/photos/_index.blade.php
+++ app/views/proprietari/apartamente/photos/_index.blade.php
@@ -6,12 +6,7 @@
 @stop
 @section('datatable-specific-page-jquery-initializations')
 		var photos = [];
-			@if(count($images) > 0) 
-			@foreach($images as $i => $photo)
-				photos.push("{{(string) Image::make($photo->file_name)->encode('data-url')}}");
-				console.log(photos);
-			@endforeach 
-			@endif
+			
 
    
 	$('.action-slider').click(function(){ 
