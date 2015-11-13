3a3de956f3c44890ae455263d68ab616021e072e
binaryk <lupacescueduard@yahoo.com>
2015-11-13 14:48:44 +0200

    Curent work

--- app/views/proprietari/apartamente/photo.blade.php
+++ app/views/proprietari/apartamente/photo.blade.php
@@ -1,1 +1,6 @@
-{{ $record->photo  }}
+@if(file_exists((string) $record->photo))
+    exista
+    @if(isset ($record->photo) )
+        <img src="{{(string) Image::make($record->photo)->encode('data-url')}}" style="width:64px"/>
+    @endif
+@endif
