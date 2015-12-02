7d3d5b9682037c65bc05126185d42e6407e22e12
binaryk <lupacescueduard@yahoo.com>
2015-12-02 14:25:09 +0200

    Curent work

--- app/config/uploads.php
+++ app/config/uploads.php
@@ -11,7 +11,7 @@ return [
 	'ansamblu-photos' => [
 		'file-name-pattern' => '{{original}}-{{id_ansamblu}}-{{date}}.{{extension}}',
 		'max-size' => 5 * 1024,
-		'allowed-extensions' => 'bmp,gif,jpeg,jpg,png,pdf',
+		'allowed-extensions' => 'bmp,gif,jpeg,jpg,png',
 		'path' => str_replace('\\', '/', storage_path()) . '/uploads/ansambluri/{{id_ansamblu}}/',
 		'id_name' => 'id_ansamblu',
 	],
@@ -25,7 +25,7 @@ return [
 	'apartament-cladire-photos' => [
 		'file-name-pattern' => '{{original}}-{{id_apartament}}-{{date}}.{{extension}}',
 		'max-size' => 5 * 1024,
-		'allowed-extensions' => 'bmp,gif,jpeg,jpg,png,pdf',
+		'allowed-extensions' => 'bmp,gif,jpeg,jpg,png',
 		'path' => str_replace('\\', '/', storage_path()) . '/uploads/cladiri/apartamente/{{id_apartament}}/',
 		'id_name' => 'id_apartament',
 	],
